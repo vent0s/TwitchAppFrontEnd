fe5132dc58f0f0f0f4f0aaa30bb67f80dd661176
vent0s <septim.siah@gmail.com>
2023-02-11 17:46:58 -0800

    last commit of front end

--- twitchfe/src/App.js
+++ twitchfe/src/App.js
@@ -1,16 +1,28 @@
-import React, { useState, useEffect } from "react";
-import { Layout, message, Menu } from "antd";
-import { LikeOutlined, FireOutlined } from "@ant-design/icons";
-import { logout, getFavoriteItem, getTopGames } from "./utils";
-import PageHeader from "./components/PageHeader";
+import { Layout, Menu, message } from "antd";
+import React, { useEffect, useState } from "react";
+import {
+  logout,
+  getTopGames,
+  getRecommendations,
+  searchGameById,
+  getFavoriteItem,
+} from "./utils";
 import CustomSearch from "./components/CustomSearch";
+import { LikeOutlined, FireOutlined } from "@ant-design/icons";
+import PageHeader from "./components/PageHeader";
+import Home from "./components/Home";
 
-const { Header, Content, Sider } = Layout;
+const { Sider, Content } = Layout;
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
-  const [favoriteItems, setFavoriteItems] = useState([]);
   const [topGames, setTopGames] = useState([]);
+  const [resources, setResources] = useState({
+    videos: [],
+    streams: [],
+    clips: [],
+  });
+  const [favoriteItems, setFavoriteItems] = useState([]);
 
   useEffect(() => {
     getTopGames()
@@ -34,6 +46,34 @@ function App() {
       .then(() => {
         setLoggedIn(false);
         message.success("Successfully Signed out");
+      })
+      .catch((err) => {
+        message.error(err.message);
+      });
+  };
+
+  const onGameSelect = ({ key }) => {
+    if (key === "recommendation") {
+      getRecommendations().then((data) => {
+        setResources(data);
+      });
+
+      return;
+    }
+
+    searchGameById(key).then((data) => {
+      setResources(data);
+    });
+  };
+
+  const customSearchOnSuccess = (data) => {
+    setResources(data);
+  };
+
+  const favoriteOnChange = () => {
+    getFavoriteItem()
+      .then((data) => {
+        setFavoriteItems(data);
       })
       .catch((err) => {
         message.error(err.message);
@@ -68,20 +108,18 @@ function App() {
 
   return (
     <Layout>
-      <Header>
-        <PageHeader
-          loggedIn={loggedIn}
-          signoutOnClick={signoutOnClick}
-          signinOnSuccess={signinOnSuccess}
-          favoriteItems={favoriteItems}
-        />
-      </Header>
+      <PageHeader
+        loggedIn={loggedIn}
+        signoutOnClick={signoutOnClick}
+        signinOnSuccess={signinOnSuccess}
+        favoriteItems={favoriteItems}
+      />
       <Layout>
         <Sider width={300} className="site-layout-background">
-          <CustomSearch onSuccess={() => {}} />
+          <CustomSearch onSuccess={customSearchOnSuccess} />
           <Menu
             mode="inline"
-            onSelect={() => {}}
+            onSelect={onGameSelect}
             style={{ marginTop: "10px" }}
             items={mapTopGamesToProps(topGames)}
           />
@@ -96,7 +134,12 @@ function App() {
               overflow: "auto",
             }}
           >
-            {"Home"}
+            <Home
+              resources={resources}
+              loggedIn={loggedIn}
+              favoriteOnChange={favoriteOnChange}
+              favoriteItems={favoriteItems}
+            />
           </Content>
         </Layout>
       </Layout>
